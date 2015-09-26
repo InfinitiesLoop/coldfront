2ec00137652f2a3a7ca518800bfb342e605b6370
Dave Reed <github@infinity88.com>
2015-09-26 10:31:05 -0700

    Initial gulp build

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+var PATHS = {
+    mainjs: "./src/main.js",
+    js: "./src/**/*.js"
+};
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: PATHS.mainjs,
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/scripts/'));
+});
