2dfebc7757ac688684a9145dcfe763f93e716355
InfinitiesLoop <infinity88@gmail.com>
2015-09-27 20:41:10 -0700

    Added gulp-rev-all support for cache busting, and ++ on code comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 'use strict';
 
-var browserify = require('browserify');
+// duh
 var gulp = require('gulp');
+
+// bundles all our files together and lets use require/import in the browser
+// Also transforms JS files in the process if you want.
+var browserify = require('browserify');
+// gulp works with vinyl streams, this is used to create a vinyl stream from browserify output
 var source = require('vinyl-source-stream');
+// todo: not sure why we need this 
 var buffer = require('vinyl-buffer');
+// minfies JS files
 var uglify = require('gulp-uglify');
+// creates sourcemaps so our LESS and JS are easily debuggable
 var sourcemaps = require('gulp-sourcemaps');
+// general utility functions like gutil.log
 var gutil = require('gulp-util');
+// lets us watch for file changes in a way that works with browserify
 var watchify = require('watchify');
+// plugs into browserify's transform stage and gives us ES6 and JSX support.
 var babelify = require('babelify');
+// lets us watch for file changes (non-broswerify)
 var watch = require('gulp-watch');
+// lets us batch up watch callbacks so we only rebuild once if there many events
 var batch = require('gulp-batch');
+// lets us process .less files into css
 var less = require('gulp-less');
+// renames files for cache-breaking, and updates references to those files in js/html/css files
+var revAll = require('gulp-rev-all');
+// deletes files, we use it to clean things up
+var del = require('del');
+// stream API we need to help clean up after gulp-rev-all
+var through = require('through2');
 
 var PATHS = {
     js: "./src/js/**/*.js",
@@ -24,6 +44,10 @@ var PATHS = {
     configjs: "./src/js/config.js",
     bundlejs: "app.js",
 };
+
+function clean() {
+    return del([PATHS.dist]);
+}
 
 function buildJs(enableWatch) {
     var b = browserify({
@@ -79,54 +103,91 @@ function buildConfig() {
         .pipe(gulp.dest(PATHS.dist + '/js'));
 }
 
-gulp.task('html', function() {
+gulp.task('clean', clean);
+
+gulp.task('html', ['clean'], function() {
     return buildHtml();
 });
-gulp.task('html-watch', function() {
+gulp.task('html-watch', ['clean'], function() {
     watch(PATHS.html, batch(function(events, done) {
         gulp.start('html', done);
     }));
     return buildHtml();
 });
 
-gulp.task('js', function() {
+gulp.task('js', ['clean'], function() {
     return buildJs();
 });
-gulp.task('js-watch', function() {
+gulp.task('js-watch', ['clean'], function() {
     return buildJs(true);
 });
 
-gulp.task('less', function() {
+gulp.task('less', ['clean'], function() {
     return buildLess();
 });
-gulp.task('less-watch', function() {
+gulp.task('less-watch', ['clean'], function() {
     watch(PATHS.less, batch(function(events, done) {
         gulp.start('less', done);
     }));
     return buildLess();
 });
 
-gulp.task('content', function() {
+gulp.task('content', ['clean'], function() {
     return buildContent();
 });
-gulp.task('content-watch', function() {
+gulp.task('content-watch', ['clean'], function() {
     watch(PATHS.content, batch(function(events, done) {
         gulp.start('content', done);
     }));
     return buildContent();
 });
 
-gulp.task('config', function() {
+gulp.task('config', ['clean'], function() {
     return buildConfig();
 });
-gulp.task('config-watch', function() {
+gulp.task('config-watch', ['clean'], function() {
     watch(PATHS.configjs, batch(function(events, done) {
         gulp.start('config', done);
     }));
     return buildConfig();
 });
 
-gulp.task('build', ['js', 'html', 'less', 'content', 'config']);
+gulp.task('rev', ['js', 'html', 'less', 'content', 'config'], function() {
+    var ra = new revAll({
+        dontRenameFile: [/^.*\/config\.js$/g, /^.*\.html$/g],
+        dontUpdateReference: [/^.*\/config\.js$/g],
+        hashLength: 5
+    });
+    return gulp.src(PATHS.dist + '/**')
+        // This will rename files with cache-breaking filenames, 
+        // and also automatically update any references to those files in css/js/html
+        .pipe(ra.revision())
+        // write the newly named files to DIST
+        .pipe(gulp.dest(PATHS.dist))
+        // lets clean up the original file names which are still there
+        .pipe(through.obj(function(file, enc, cb) {
+            // for each file, if the originalPath is difference than the current one
+            // then it was a renamed file. Delete the original.
+            if (file.revOrigPath !== file.path) {
+                del(file.revOrigPath);
+            }
+            return cb();
+        }));
+});
+
+// 'gulp build' or just 'gulp' is what you would normally run to just get a build output.
+// This runs the whole process as well as rev-all(), which creates cache-busting file names
+// for static assets.
+gulp.task('build', ['rev']);
+gulp.task('default', ['build']);
+
+// 'gulp watch' will build everything and then watch for changes to js/less/html, and rebuild
+// whatever needs to be rebuild if there are changes. Note that this does NOT use rev-all()
+// since you can't really incrementally update assets without rebuilding the whole thing,
+// since a change to a JS file, for example, will cause there to be a new file name, which
+// means the HTML file referencing it must change, too.
+// TODO: Maybe just simplify and make watch rebuild everything every time
 gulp.task('watch', ['js-watch', 'html-watch', 'less-watch', 'content-watch', 'config-watch']);
-gulp.task('default', ['watch']);
+
+
 
